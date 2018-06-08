e1280f31779c4b267ab9b484f1eb1907f7731d21
edu <vizuete.eduardo@gmail.com>
2018-06-08 18:33:38 -0500

    Integration test e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('musical-lake App', () => {
   let page: AppPage;
@@ -7,8 +8,26 @@ describe('musical-lake App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Musical Lake App!');
   });
+
+  it('should display animal list', () => {
+    const listAnimals = [ '1 frog', '2 dragonfly', '3 criket' ];
+    expect(page.getAnimalList()).toEqual(listAnimals);
+  });
+
+  it('should display frog details', () => {
+    page.clicAnimalLink();
+    expect(page.getTextDetail()).toEqual('FROG Details');
+  });
+
+  it('should display next sounds', () => {
+    page.selectSoundList();
+    page.clicButtonSearch();
+    expect(page.getTextSearch()).toEqual('==> fiu');
+    browser.sleep(5000);
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,31 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getAnimalList() {
+    return element.all(by.css('app-animals li')).getText();
+  }
+
+  clicAnimalLink() {
+    // element(by.partialLinkText('fro')).click();
+    // element(by.css('app-animals li')).click();
+    element.all(by.css('app-animals li')).get(0).click();
+  }
+
+  getTextDetail() {
+    return element(by.css('app-animal-detail h2')).getText();
+  }
+
+  selectSoundList() {
+    element.all(by.cssContainingText('option', 'brr')).get(0).click();
+  }
+
+  clicButtonSearch() {
+    element(by.css('button')).click();
+  }
+
+  getTextSearch() {
+    return element(by.xpath('//div[contains(text(), "==>")]')).getText();
+  }
+
 }
